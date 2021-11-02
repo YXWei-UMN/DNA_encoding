428969d29537e1777475077da782686c45eba124
wwl2755 <wangwenlong2755@gmail.com>
2021-11-02 15:13:15 -0500

    do exhausted search among cut-mapping combinations

--- hybrid_area/shell.sh
+++ hybrid_area/shell.sh
@@ -1,6 +1,2 @@
-g++ -std=c++11 -o hybrid hybrid.cpp
-#./hybrid blast_result_evalue50 blast_result_evalue50 blast_result_evalue50 blast_result_evalue50
-#./hybrid file.txt file.txt file.txt file.txt
-#./hybrid test test1 test2 test3
-#./hybrid blast_PDF_GF47 blast_PDF_trans_1 blast_PDF_trans_2 blast_PDF_trans_3
-./hybrid mapping0/ mapping1/ mapping2/ mapping3/
+g++ -std=c++11 -o hybrid_new hybrid_new.cpp
+./hybrid_new mapping0/ mapping1/ mapping2/ mapping3/
