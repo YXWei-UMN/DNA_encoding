c412285806f18a74dd898f707ff2b441b8dbd93b
Huibing Dong <huibingdong@Huibings-MacBook-Pro.local>
2022-07-13 22:34:18 -0700

    fixed a len to 200

--- tools/var_len_combine.cpp
+++ tools/var_len_combine.cpp
@@ -9,17 +9,16 @@ using namespace std;
 const int MINN = 100;
 const int MAXN = 200;
 
-int last = 200;
-int min_blind_spot = 200;
-int min_uncovered = 200;
+int min_blind_spot = 296;
+int min_uncovered = 187;
 
-int len[4] = {100, 110, 132, 144};
+int len[4] = {100, 110, 132, 200};
 
 int calc_blind_spot() {
   vector<int> com(len, len+4);
   unordered_set<int> s(len, len+4);
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 6; i++) {
     for (int x: com) {
       for (int y: com) {
         s.insert(x+y);
@@ -28,14 +27,14 @@ int calc_blind_spot() {
 
     com.clear();
     for (auto it = s.begin(); it != s.end(); it++) {
-      if (*it <= 450) {
+      if (*it <= min_blind_spot+300) {
         com.push_back(*it);
       } 
     }
     sort(com.begin(), com.end());
   }
 
-  for (int i = 200; i <= 400; i++) {
+  for (int i = min_blind_spot; i <= min_blind_spot+200; i++) {
     bool found = false;
     for (int j = i - 12; j <= i; j++) {
       if (s.find(j)!=s.end()){
@@ -63,16 +62,16 @@ int calc_blind_spot() {
 
     if (com[i] - com[i-1] > 12) {
       cur_uncovered += com[i] - com[i-1] -12;
-      if (cur_uncovered >= min_uncovered) return -1;
+      if (cur_uncovered > min_uncovered) return -1;
       contiguous_start = com[i];
-      if (contiguous_start > 200) return -1;
+      if (contiguous_start > min_blind_spot) return -1;
     } else {
       int contiguous_length = com[i] - contiguous_start + 1;
       if (contiguous_length >= 200) {
-        min_uncovered = min(min_uncovered, cur_uncovered);
-        if (min_uncovered < last) {
-          last = min_uncovered;
-          cout << "[uncovered=" << cur_uncovered << "] ";
+        if (cur_uncovered <= min_uncovered || contiguous_start <= min_blind_spot){
+          min_blind_spot = min(min_blind_spot, contiguous_start);
+          min_uncovered = min(min_blind_spot, cur_uncovered);
+          cout << "[blind_spot, uncovered] = " << "[" << contiguous_start << "," << cur_uncovered << "]\n";
           // for (int i = 0; i < 4; i++) cout << com[i] << " "; cout << endl;
           for (auto i = 0; i < com.size() && i < 20; i++) cout << com[i] << " "; cout << endl;
         }
@@ -86,7 +85,7 @@ int calc_blind_spot() {
 }
 
 void dfs(int step = 0) {
-  if (step == 4) {
+  if (step == 3) {
     calc_blind_spot();
     // exit(0);
     return;
